a8596adea551ae96bf296cc26067da81b4426472
mangila <olsson.erik1993@gmail.com>
2024-10-05 23:40:38 +0200

    init commit

---
+++ src/isodd/is_odd_test.go
@@ -0,0 +1,25 @@
+package isodd
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsOdd(t *testing.T) {
+	numbersOdd := []int{1, 3, 5, 7, 9}
+	numbersEven := []int{0, 2, 4, 6, 8}
+
+	for _, v := range numbersOdd {
+		if !IsOdd(v) {
+			fmt.Println(v)
+			t.Error("Number is not odd!")
+		}
+	}
+
+	for _, v := range numbersEven {
+		if IsOdd(v) {
+			fmt.Println(v)
+			t.Error("Number is not odd!")
+		}
+	}
+}
